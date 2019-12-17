8cf5ae3fa1f737beec673adbb4fb326affb29fa8
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-17 13:13:05 -0500

    restricted middleware

--- api/server.js
+++ api/server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfiguration = {
+	//session storage options
+	name             : 'choclatechip', //default would be sid
+	secret           : 'keep it secret, keep it safe', //used for encryption (must be an environment variable)
+	saveUnintialized : true, //has implications with GDPR laws
+	resave           : false,
+
+	//cookie options
+	cookie           : {
+		maxAge   : 1000 * 60 * 10, //10 mins in milliseconds
+		secure   : false, //if false the cooke is sent over http, if true only sent over https
+		httpOnly : true, //if true JS cannot access the cookie
+	},
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionConfiguration)); //with add a req.session object
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+	res.json({ api: 'up' });
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,36 +5,38 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+	user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+	Users.add(user)
+		.then((saved) => {
+			res.status(201).json(saved);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+	let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+	Users.findBy({ username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				//save a session for the client and send back a cookie
+				req.session.user = user;
+				res.status(200).json({
+					message : `Welcome ${user.username}!`,
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
 });
 
 module.exports = router;
